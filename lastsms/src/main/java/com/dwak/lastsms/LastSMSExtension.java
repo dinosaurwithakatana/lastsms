d7598de94d2307b745ade82c713f65720d776e2a
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2013-12-22 21:50:06 -0600

    adding first files

---
+++ lastsms/src/main/java/com/dwak/lastsms/LastSMSExtension.java
@@ -0,0 +1,14 @@
+package com.dwak.lastsms;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+
+public class LastSMSExtension extends DashClockExtension {
+
+    private static final String TAG = LastSMSExtension.class.getSimpleName();
+
+    @Override
+    protected void onUpdateData(int arg0) {
+
+    }
+
+}
